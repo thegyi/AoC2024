c276bdac632c6dc073b5af950f6d06bf8edfbe7e
Tamas Hegyi <40230373+thegyi@users.noreply.github.com>
2024-12-02 09:25:28 +0100

    add comments

--- 2_2.cpp
+++ 2_2.cpp
@@ -5,11 +5,21 @@
 #include <fstream>
 #include <functional>
 #include <iostream>
+#include <iterator>
 #include <map>
 #include <string>
 #include <vector>
 
-std::vector<std::string> split(std::string str, std::string delimiter) {
+/**
+ * @brief Splits a string into a vector of strings using a delimiter.
+ *
+ * @param str The string to split.
+ * @param delimiter The delimiter to split by.
+ *
+ * @return A vector of strings, each representing a substring of @p str
+ *         separated by @p delimiter.
+ */
+std::vector<std::string> split(std::string str, const std::string delimiter) {
   size_t pos = 0;
   std::string token;
   std::vector<std::string> result;
@@ -22,7 +32,18 @@ std::vector<std::string> split(std::string str, std::string delimiter) {
   return result;
 };
 
-bool isIncreasing(std::vector<std::string> v) {
+/**
+ * @brief Checks if a sequence of numbers is strictly increasing.
+ *
+ * Iterates through a vector of strings representing numbers and
+ * verifies that each number is greater than the preceding number.
+ *
+ * @param v A vector of strings representing a sequence of numbers.
+ *
+ * @return @c true if each number is greater than the preceding number,
+ *         @c false otherwise.
+ */
+bool isIncreasing(std::vector<std::string> &v) {
   for (unsigned int i = 1; i < v.size(); i++) {
     if (std::stoi(v[i]) <= std::stoi(v[i - 1])) {
       return false;
@@ -31,7 +52,16 @@ bool isIncreasing(std::vector<std::string> v) {
   return true;
 }
 
-bool isDecreasing(std::vector<std::string> v) {
+/**
+ * @brief Checks if a sequence of numbers is strictly decreasing.
+ *
+ * Iterates through a vector of strings representing numbers and
+ * verifies that each number is less than the preceding number.
+ *
+ * @param v A vector of strings representing a sequence of numbers.
+ * @return true if the sequence is strictly decreasing, false otherwise.
+ */
+bool isDecreasing(std::vector<std::string> &v) {
   for (unsigned int i = 1; i < v.size(); i++) {
     if (std::stoi(v[i]) >= std::stoi(v[i - 1])) {
       return false;
@@ -40,7 +70,15 @@ bool isDecreasing(std::vector<std::string> v) {
   return true;
 }
 
-bool isSafe(std::vector<std::string> s) {
+/**
+ * @brief Checks if a sequence of numbers is "safe" according to the following
+ * rules.
+ *
+ * A sequence is "safe" if it is either increasing or decreasing and the
+ * absolute difference between any two consecutive numbers is never greater than
+ * 3 and never less than 1.
+ */
+bool isSafe(std::vector<std::string> &s) {
   for (unsigned int i = 1; i < s.size(); i++) {
     int tmp = std::stoi(s[i]);
     int tmp2 = std::stoi(s[i - 1]);
@@ -60,7 +98,7 @@ int main() {
   while (std::getline(in, line)) {
     auto numbers = split(line, " ");
     for (std::size_t i = 0; i < numbers.size(); i++) {
-      std::vector<std::string> numbers2 = numbers;
+      std::vector<std::string> numbers2(numbers);
       numbers2.erase(numbers2.begin() + i);
       if (isSafe(numbers2)) {
         countOfSafeReports++;
